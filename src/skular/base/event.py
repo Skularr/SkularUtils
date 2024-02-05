b70af07f9399fed233ca6703493478f73d0d8943
JRudransh <jrudransh@protonmail.com>
2024-02-05 20:18:04 +0530

    Add abstractmethod

--- src/skular/base/event.py
+++ src/skular/base/event.py
@@ -1,6 +1,12 @@
+from abc import ABC, abstractmethod
+
 from ..models.qdata import QData
 
 
-class BaseHandler:
+class BaseHandler(ABC):
     def __init__(self, data: dict):
         self._data: QData = data
+
+    @abstractmethod
+    async def __call__(self):
+        ...
